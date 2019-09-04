736b94b1e387a0b92b453416e309b6fba4dfd161
swami-devops <schwami.iyer@gmail.com>
2019-09-04 04:47:38 +0000

    pvt docker

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,9 +14,9 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('microswami/nodejs')
+            repositoryName('krepo:5000/nodejs')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
+            registryCredentials('krepo')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
